125dbdc9deacec2076feb24c73a35a4ea44b8706
Artyom Borisov <2001raf@gmail.com>
2025-08-10 18:39:25 +0500

    Update datasets_whu_train

--- scripts/datasets_whu_train.sh
+++ scripts/datasets_whu_train.sh
@@ -1,19 +1,13 @@
 #!/bin/sh
 
-# Initialize conda without conda init
-eval "$(conda shell.bash hook)"
-conda activate monster
-export PYTHONPATH=$(pwd)
-
 CPU_TRAIN=false
 
 SBATCH_CPU=""
-PYTHON_CPU=""
 if [ $CPU_TRAIN == false ]
 then
   SBATCH_CPU="-p hiperf --gres=gpu:4"
 else
-  PYTHON_CPU="--cpu"
+  SBATCH_CPU="-p tesla"
 fi
 
 sbatch -n1 \
@@ -25,4 +19,4 @@ $SBATCH_CPU \
 --output=./logs/datasets_whu_train_%j.log \
 --error=./logs/datasets_whu_train_%j.err \
 \
---wrap="CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch train_whu.py"
+--wrap="PATH=/home/s0214/.conda/envs/monster/bin:\$PATH CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --num_processes=4 --num_machines=1 --num_processes_per_machine=4 python train_whu.py"
